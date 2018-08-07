7e59b4a67631e4ea7ef266bbe65c0abcb80069bf
Unknown <vladimirmbassi@gmail.com>
2018-08-07 13:10:32 +0200

    working test version ios

--- ios/Classes/SwiftAuth0Plugin.swift
+++ ios/Classes/SwiftAuth0Plugin.swift
@@ -1,14 +1,70 @@
 import Flutter
 import UIKit
-    
-public class SwiftAuth0Plugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "auth0", binaryMessenger: registrar.messenger())
-    let instance = SwiftAuth0Plugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+import Auth0
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+public class SwiftAuth0Plugin: NSObject, FlutterPlugin {
+    var credentialsManager = CredentialsManager(authentication: Auth0.authentication())
+
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "auth0", binaryMessenger: registrar.messenger())
+        let instance = SwiftAuth0Plugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let method = call.method
+        let dic = call.arguments as? [String: Any]
+        
+        switch(method) {
+        case "login":
+            let audience = dic!["audience"] as! String
+            login(audience: audience)
+            break
+        case "getToken":
+            getAccessToken(result: result)
+            break
+        case "isLoggedIn":
+            result(getIsLoggedIn())
+            break
+        default:
+            result(FlutterMethodNotImplemented)
+            break
+        }
+        
+    }
+    
+    private func getIsLoggedIn() -> Bool {
+        return credentialsManager.hasValid()
+    }
+    
+    private func getAccessToken(result: @escaping FlutterResult) {
+        credentialsManager.credentials { error, credentials in
+            guard error == nil, let credentials = credentials else {
+                result(error)
+                return
+            }
+            // Valid credentials, you can access the token properties such as `idToken`, `accessToken`.
+            result(credentials.accessToken)
+        }
+    }
+    
+    // Login with audience
+    private func login(audience: String) {
+        Auth0.webAuth()
+            .scope("openid profile offline_access")
+            .audience(audience)
+            .start { result in
+                switch result {
+                case .success(let credentials):
+                    self.credentialsManager.store(credentials: credentials)
+                    print("Auth0: connected")
+                    break
+                case .failure(let error):
+                    print(error)
+                    break
+                }
+        }
+    }
+    
 }
